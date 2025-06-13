b4b17f165b6f07a8f7ca6792ae0f774a3c4a2486
Chris <christo_rivi@hotmail.com>
2025-06-13 13:00:26 -0600

    Monitor

--- src/App.js
+++ src/App.js
@@ -15,19 +15,14 @@ import BotonGraficar from './graficar';
 
 function App() {
   const [dato, setDato] = useState({ uso_cpu: null, uso_memoria: null, uso_disco: null, uso_gpu: null, });
-  const [GraficoCPU, setGraficoCPU] = useState(false);
-  const [GraficoDisco, setGraficoDisco] = useState(false);
-  const [GraficoMemoria, setGraficoMemoria] = useState(false);
-  const [GraficoGPU, setGraficoGPU] = useState(false);
-
-
+  const [graficoVisible, setGraficoVisible] = useState(null);
   
 useEffect(() => {
     const fetchData = async () => {
     const res = await fetch("http://localhost:8080/rendimientos/envio_datos.php");
     const datos = await res.json();
 
-    setDato({uso_cpu: datos.uso_cpu, uso_memoria: datos.uso_memoria, uso_disco: datos.uso_disco, uso_gpu: datos.uso_gpu,});
+    setDato({uso_cpu: datos.uso_cpu, uso_memoria: datos.uso_memoria, uso_disco: datos.uso_disco, uso_gpu: datos.uso_gpu});
   };
 
     fetchData(); 
@@ -44,18 +39,17 @@ useEffect(() => {
       <header className="App-header">
         <h3>Administrador de Tareas:</h3>
         <Table cpu={dato.uso_cpu} memoria={dato.uso_memoria} disco={dato.uso_disco} gpu={dato.uso_gpu}/>
-        <BotonCPU onClick={() => setGraficoCPU(prev => !prev)} />
-        <BotonMemoria onClick={() => setGraficoMemoria(prev => !prev)}/>
-        <BotonDisco onClick={() => setGraficoDisco(prev => !prev)}/>
-        <BotonGPU onClick={() => setGraficoGPU(prev => !prev)}/>
+        <BotonCPU onClick={() => setGraficoVisible("CPU")} />
+        <BotonMemoria onClick={() => setGraficoVisible("Memoria")} />
+        <BotonDisco onClick={() => setGraficoVisible("Disco")} />
+        <BotonGPU onClick={() => setGraficoVisible("GPU")} />
         <Horas1 />
         <Horas2 />
         <Dias/>
-        <BotonGraficar/>
-        {GraficoCPU && <Grafico cpu={dato.uso_cpu} />}
-        {GraficoDisco && <Grafico disco={dato.uso_disco} />}
-        {GraficoMemoria && <Grafico memoria={dato.uso_memoria} />}
-        {GraficoGPU && <Grafico gpu={dato.uso_gpu} />}
+        {graficoVisible === "CPU" && <Grafico cpu={dato.uso_cpu} />}
+        {graficoVisible === "Memoria" && <Grafico memoria={dato.uso_memoria} />}
+        {graficoVisible === "Disco" && <Grafico disco={dato.uso_disco} />}
+        {graficoVisible === "GPU" && <Grafico gpu={dato.uso_gpu} />}
       </header>
     </div>
   );
--- src/graficar.jsx
+++ src/graficar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function BotonGraficar({ onClick }) {
+export default function BotonGraficar({onClick}) {
 
   return (
   <header>
